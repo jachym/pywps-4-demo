1570f343c793a63b5e869ecd65547fb18e3fa7bb
Jachym Cepicky <jachym.cepicky@gmail.com>
2015-07-19 19:53:04 +0200

    execute tests

---
+++ tests/common.py
@@ -0,0 +1,35 @@
+import lxml.etree as etree
+import sys
+import urllib
+PY2 = sys.version_info[0] == 2
+
+if not PY2:
+    import urllib.request
+
+def get_response(url):
+
+    response = None
+
+    if PY2:
+        response = urllib.urlopen(url)
+    else:
+        response = urllib.request.urlopen(url)
+
+    return response
+
+def get_schema(url):
+
+    schema = get_response(url)
+    xmlschema_doc = etree.parse(schema)
+    return etree.XMLSchema(xmlschema_doc)
+
+def validate(url, schema):
+    response = get_response(url)
+    info = response.info()
+    body = response.read()
+    body_doc = etree.fromstring(body)
+    
+    schema = get_schema(schema)
+
+    return schema.validate(body_doc)
+
